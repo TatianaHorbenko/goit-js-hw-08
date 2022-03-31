2270b48d9ec889843934739bba9ec6f8c40e631f
TatianaHorbenko <3435640@gmail.com>
2022-03-31 14:07:41 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const imageGallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+renderGallery();
+
+function renderGallery() {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  `,
+    )
+    .join('');
+
+  imageGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const storageKey = 'feedback-form-state';
+
+const formData = {};
+
+function onFormInput(event) {
+  formData.message = form.elements.message.value;
+  formData.email = form.elements.email.value;
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+formRefresh();
+
+function formRefresh() {
+  const savedData = localStorage.getItem(storageKey);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+  event.currentTarget.reset();
+  localStorage.removeItem(storageKey);
+
+  console.log(formData);
+}
